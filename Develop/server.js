736102a6ce69468e62c56e8bf0c44724d59ab231
Kirsten Nelson <kirmarnel@gmail.com>
2021-04-19 18:28:07 -0700

    establish api routes

--- Develop/server.js
+++ Develop/server.js
@@ -9,19 +9,42 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //HTML routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
 
 //Api routes to write notes
-app.post('/api/notes', (res, req)=> {
-    console.log(req.body)
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body
 
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        let noteInfo = JSON.parse(data)
+        noteInfo.push(newNote)
     
-    res,end();
-})
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(noteInfo), (err, data) => {
+            if (err) throw err;
+            res.send()
+        });     
+    });   
+});
+
 //Api routes to get notes
+app.get("/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        let noteInfo = JSON.parse(data)
+        res.json(noteInfo)
+        console.log(noteInfo)
+    })
+});
 
 
 
